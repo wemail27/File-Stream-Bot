515f34daf2945def3e03084f7c865946097c2905
wemail27 <64365096+wemail27@users.noreply.github.com>
2024-09-04 00:57:44 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,13 +11,13 @@ class Var(object):
     API_ID = int(getenv('API_ID', ''))
     API_HASH = str(getenv('API_HASH', '30fa091455c0548d77dc254f0bb705b0'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6195871057:AAGzevzkPgPKoaYFHisKWeItUpStmgbM0Wg:'))
-    name = str(getenv('name', 'File-Stream-Bot'))
+    name = str(getenv('name', 'Filestoreogbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001815998255'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "20"))  # 20 minutes
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "875770605").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
